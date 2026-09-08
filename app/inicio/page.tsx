eeebf8733d955873990d6660a8f6c0e2ca074db6
tracbel25-dev <tracbel25@gmail.com>
2026-09-07 22:37:00 -0300

    feat: add corporate landing route

---
+++ app/inicio/page.tsx
@@ -0,0 +1,5 @@
+import { Header } from '@/components/Header';
+import { Footer } from '@/components/Footer';
+import { HomeExperience } from '@/components/HomeExperience';
+
+export default function Inicio(){return <><Header/><main><HomeExperience/></main><Footer/></>}
