893fcbe58e571d87c8741e9250a362cb41ecff4d
tracbel25-dev <tracbel25@gmail.com>
2026-09-08 13:08:11 -0300

    feat: cria rota para nova senha

---
+++ app/nova-senha/page.tsx
@@ -0,0 +1,5 @@
+import { NewPasswordForm } from '@/components/PasswordRecovery';
+import '../auth-local.css';
+
+export const metadata={title:'Nova senha | CRM PLUS Store'};
+export default function NewPassword(){return <NewPasswordForm/>;}
