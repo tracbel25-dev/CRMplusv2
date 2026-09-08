9d8c8c344a6d6fbabb4a5d10b05df6b655c846f8
tracbel25-dev <tracbel25@gmail.com>
2026-09-08 16:35:35 -0300

    Carrega estilos editoriais na home

--- app/inicio/page.tsx
+++ app/inicio/page.tsx
@@ -1,6 +1,7 @@
 import { Header } from '@/components/Header';
 import { Footer } from '@/components/Footer';
 import { HomeExperience } from '@/components/HomeExperience';
+import './cover.css';
+import './editorial.css';
 
 export default function Inicio(){return <><Header/><main><HomeExperience/></main><Footer/></>}
-import './cover.css';
