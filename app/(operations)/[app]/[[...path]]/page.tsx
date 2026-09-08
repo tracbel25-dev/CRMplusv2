e6d5bd73cd2da0ab0d7af66e4f30dc54f6de8da1
tracbel25-dev <tracbel25@gmail.com>
2026-09-08 20:57:59 -0300

    Aplica marca do Artemis dentro do app

--- app/(operations)/[app]/[[...path]]/page.tsx
+++ app/(operations)/[app]/[[...path]]/page.tsx
@@ -6,6 +6,7 @@ import './operations.css';
 import './configuration.css';
 import './access-control.css';
 import './zeus-brand.css';
+import './artemis-brand.css';
 
 export const dynamicParams = true;
 
