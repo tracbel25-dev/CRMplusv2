ad07dfbf08ff41a17051dcff2561efed6614a343
tracbel25-dev <tracbel25@gmail.com>
2026-09-14 22:02:49 -0300

    Direciona cardápio externo Artemis para nova experiência

--- app/externo/[token]/page.tsx
+++ app/externo/[token]/page.tsx
@@ -1,7 +1,7 @@
-import { ExternalPublic } from '@/components/external/ExternalPublic';
+import { ExternalRouter } from '@/components/external/ExternalRouter';
 import '../external.css';
 
 export default async function ExternalPage({ params }: { params: Promise<{ token: string }> }) {
   const { token } = await params;
-  return <ExternalPublic token={token} />;
+  return <ExternalRouter token={token} />;
 }
