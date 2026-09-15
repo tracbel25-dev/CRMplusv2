367be1c07922d506382222d969e259c6ee8e3642
tracbel25-dev <tracbel25@gmail.com>
2026-09-15 20:40:21 -0300

    feat(zeus): mostra vínculos permanentes dentro da OS

--- components/operations/LeanZeusJobDetail.tsx
+++ components/operations/LeanZeusJobDetail.tsx
@@ -3,7 +3,13 @@
 import type { Workspace } from '@/lib/operations/storage';
 import { LeanZeusJobDetail as LeanZeusJobDetailBase } from './LeanZeusJobDetailBase';
 import { ZeusIdentificationChecklist } from './ZeusIdentificationChecklist';
+import { ZeusAttachmentMetadataBridge, ZeusJobOperationalRecord } from './ZeusJobBindings';
 
 export function LeanZeusJobDetail({ w, recordId }: { w: Workspace; recordId: string }) {
-  return <><LeanZeusJobDetailBase w={w} recordId={recordId} /><ZeusIdentificationChecklist w={w} jobId={recordId} /></>;
+  return <>
+    <LeanZeusJobDetailBase w={w} recordId={recordId} />
+    <ZeusIdentificationChecklist w={w} jobId={recordId} />
+    <ZeusAttachmentMetadataBridge w={w} jobId={recordId} />
+    <ZeusJobOperationalRecord w={w} jobId={recordId} />
+  </>;
 }
