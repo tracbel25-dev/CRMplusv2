3b9953992a9a3dbd60be7846c8e086ce2fcbd107
tracbel25-dev <tracbel25@gmail.com>
2026-09-19 01:09:48 -0300

    fix(zeus): bloqueia checklist fora do plano

--- components/operations/LeanZeusJobDetail.tsx
+++ components/operations/LeanZeusJobDetail.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import type { Workspace } from '@/lib/operations/storage';
+import { zeusViewHasFeature } from '@/lib/operations/zeusPlans';
 import { LeanZeusJobDetail as LeanZeusJobDetailBase } from './LeanZeusJobDetailBase';
 import { ZeusIdentificationChecklist } from './ZeusIdentificationChecklist';
 import { ZeusAttachmentMetadataBridge, ZeusJobOperationalRecord } from './ZeusJobBindings';
@@ -9,7 +10,7 @@ import { ZeusOperatorFeedback } from './ZeusOperatorFeedback';
 export function LeanZeusJobDetail({ w, recordId }: { w: Workspace; recordId: string }) {
   return <ZeusOperatorFeedback w={w} jobId={recordId}>
     <LeanZeusJobDetailBase w={w} recordId={recordId} />
-    <ZeusIdentificationChecklist w={w} jobId={recordId} />
+    {zeusViewHasFeature(w.data.settings, 'checklist') && <ZeusIdentificationChecklist w={w} jobId={recordId} />}
     <ZeusAttachmentMetadataBridge w={w} jobId={recordId} />
     <ZeusJobOperationalRecord w={w} jobId={recordId} />
   </ZeusOperatorFeedback>;
